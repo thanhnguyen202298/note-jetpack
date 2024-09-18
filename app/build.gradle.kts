7176cae027a0149530ee7f2752d24d96138fd165
thanhnguyendirox <thanh.nguyen@dirox.net>
2024-09-18 22:00:57 +0700

    koroutine Events

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,11 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.koin.core.coroutines)
+    // ViewModel
+    val lifecycle_version = "2.8.5"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 }
